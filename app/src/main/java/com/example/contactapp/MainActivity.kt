786514ab685493f3d85225b19e24fbf936777a91
Ivy-aminga <amingaivy@gmail.com>
2023-06-15 21:08:29 +0300

    contacts app

--- app/src/main/java/com/example/contactapp/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/MainActivity.kt
@@ -27,19 +27,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displayContacts(){
-        var name1 = ContactData("Emmily","emmily@gmail.com","79072548748","https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
-        var name2 = ContactData("Stephanie","stephanie@gmail.com","79072548748","https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
-        var name3 = ContactData("Annet","annett@gmail.com","79072548748","https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
-        var name4 = ContactData("Mercy","mercy@gmail.com","79072548748","https://media.istockphoto.com/id/1381203194/photo/beautiful-afro-woman.jpg?s=612x612&w=0&k=20&c=6Klilmbsd4buHLE1QkWYg_duPsoMtQ9XrOwSvL6Xir0=")
-        var name5 = ContactData("John","john@gmail.com","79072548748", "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
-        var name6= ContactData("Jane","jane@gmail.com","79072548748","https://media.istockphoto.com/id/1408224350/photo/photo-of-cheerful-dark-skin-girl-dance-cool-wear-summer-outfit-raise-hands-isolated-on-funky.jpg?s=612x612&w=0&k=20&c=Jtqzmi7Lbpz9qRQfpIrOE5D_Wuaw3KH2AUw_0W3nJuE=")
-        var name7 = ContactData("Magret","magret@gmail.com","79072548748","https://media.istockphoto.com/id/1255950210/photo/close-up-portrait-of-happy-african-american-woman-looking-at-camera-with-mouth-open.jpg?s=612x612&w=0&k=20&c=YBIkv7xW_Cz0pCCSTwIl3UYKXkva49H-yIcj5IcFfps=")
-        var name8 = ContactData("Leila","leila@gmail.com","79072548748","https://media.istockphoto.com/id/1426200383/photo/charming-african-american-young-woman-listen-music-track-isolated-on-white-background-afro.jpg?s=612x612&w=0&k=20&c=C8cN8hXVAZ2V6dxkNQZI1USGJp11UVq6ddGyKRU4iyQ=")
-        var name9 = ContactData("Ayla","ayla@gmail.com","79072548748","https://media.istockphoto.com/id/928419104/photo/beauty-portrait-of-young-african-american-girl-with-afro-hairstyle-girl-posing-on-yellow.jpg?s=612x612&w=0&k=20&c=OP3qrkArCaXP99kqGy7ADL6QA8XBPrnbXNNykCUEnJk=")
-        var name10 = ContactData("Yvonne","yvonne@gmail.com","79072548748","https://media.istockphoto.com/id/1293629947/photo/young-african-american-woman-smiling-posing-on-yellow-background.jpg?s=612x612&w=0&k=20&c=7csvY2FQuY2o4D8C38qzpvOiUQcWribnVhRZHK4jMy4=" )
+        var name1 = ContactData("Ivy","ivy@gmail.com","071234567","https://media.istockphoto.com/id/1381203194/photo/beautiful-afro-woman.jpg?s=612x612&w=0&k=20&c=6Klilmbsd4buHLE1QkWYg_duPsoMtQ9XrOwSvL6Xir0=")
+        var name2 = ContactData("Aming'a","aminga@gmail.com","071234567","https://media.istockphoto.com/id/1293629947/photo/young-african-american-woman-smiling-posing-on-yellow-background.jpg?s=612x612&w=0&k=20&c=7csvY2FQuY2o4D8C38qzpvOiUQcWribnVhRZHK4jMy4=")
+        var name3 = ContactData("Marrion","marrion@gmail.com","071234567","https://media.istockphoto.com/id/1426200383/photo/charming-african-american-young-woman-listen-music-track-isolated-on-white-background-afro.jpg?s=612x612&w=0&k=20&c=C8cN8hXVAZ2V6dxkNQZI1USGJp11UVq6ddGyKRU4iyQ=")
+        var name4 = ContactData("Mideva","mideva@gmail.com","071267899","https://media.istockphoto.com/id/1381203194/photo/beautiful-afro-woman.jpg?s=612x612&w=0&k=20&c=6Klilmbsd4buHLE1QkWYg_duPsoMtQ9XrOwSvL6Xir0=")
+        var name5 = ContactData("Aniso","aniso@gmail.com","07689654", "https://media.istockphoto.com/id/1408224350/photo/photo-of-cheerful-dark-skin-girl-dance-cool-wear-summer-outfit-raise-hands-isolated-on-funky.jpg?s=612x612&w=0&k=20&c=Jtqzmi7Lbpz9qRQfpIrOE5D_Wuaw3KH2AUw_0W3nJuE=")
+        var name6= ContactData("Kilas","kilas@gmail.com","0724678863","https://media.istockphoto.com/id/1408224350/photo/photo-of-cheerful-dark-skin-girl-dance-cool-wear-summer-outfit-raise-hands-isolated-on-funky.jpg?s=612x612&w=0&k=20&c=Jtqzmi7Lbpz9qRQfpIrOE5D_Wuaw3KH2AUw_0W3nJuE=")
+        var name7 = ContactData("Pauline","pauline@gmail.com","073456782","https://media.istockphoto.com/id/1255950210/photo/close-up-portrait-of-happy-african-american-woman-looking-at-camera-with-mouth-open.jpg?s=612x612&w=0&k=20&c=YBIkv7xW_Cz0pCCSTwIl3UYKXkva49H-yIcj5IcFfps=")
+        var name8 = ContactData("Okoth","okoth@gmail.com","0790725487","https://media.istockphoto.com/id/1426200383/photo/charming-african-american-young-woman-listen-music-track-isolated-on-white-background-afro.jpg?s=612x612&w=0&k=20&c=C8cN8hXVAZ2V6dxkNQZI1USGJp11UVq6ddGyKRU4iyQ=")
+        var name9 = ContactData("Clever","clever@gmail.com","0735679890","https://media.istockphoto.com/id/928419104/photo/beauty-portrait-of-young-african-american-girl-with-afro-hairstyle-girl-posing-on-yellow.jpg?s=612x612&w=0&k=20&c=OP3qrkArCaXP99kqGy7ADL6QA8XBPrnbXNNykCUEnJk=")
+        var name10 = ContactData("Nyeliep","nyeliep@gmail.com","0787487689","https://media.istockphoto.com/id/1293629947/photo/young-african-american-woman-smiling-posing-on-yellow-background.jpg?s=612x612&w=0&k=20&c=7csvY2FQuY2o4D8C38qzpvOiUQcWribnVhRZHK4jMy4=" )
         var names = listOf<ContactData>(name1,name2, name3,name4,name5,name6,name7,name8,name9,name10)
         binding.rvContact.layoutManager = LinearLayoutManager(this)
         val contactsAdapter =ContactsAdapter(names)
 //        var contactsAdapter = ContactsRVAdapter(contactList)
         binding.rvContact.adapter = contactsAdapter}}
 //
+
